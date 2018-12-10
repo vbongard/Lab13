e71aa8ae4624804d140e4c387f1f2ade125dd097
Linferno <linfuz88@gmail.com>
2018-12-10 16:28:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 <section>
     There are many cool instruments out there. Learn about the more fascinating ones by going through our menu! Which instrument is your favorite?
 </section>
+<img src="images/orchestra.jpg" alt="orchestra image">
 <!-- So I'm thinking for the index page, the survey should be at the bottom (terminal) area, and if the user
 chooses not to click, it will move into the header for the other pages. -->
 <a href="survey.php">Take our survey!</a>   
