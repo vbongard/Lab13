eea859f10e67182c6ca952a7f29cd4ff455a9fe9
Jonathan Roman <jroman8@u.rochester.edu>
2018-12-10 17:01:26 -0500

    design changes pt.3

--- survey.php
+++ survey.php
@@ -1,10 +1,12 @@
 <?php include "inc/html.php"; ?>
 <form  action="afterSurvey.php" method="get">
+	<div class="fullForm">
     Full name: <input type="text" name="name"><br>
-    Have you heard of any of these instruments before? <input type="checkbox" name="heard"><br>
+   	Have you heard of any of these instruments before? <input type="checkbox" name="heard"><br>
     What was your favorite instrument of the three? <br><input type="radio" name="fav" value="theremin"> Theremin <br><input type="radio" name="fav" value="harmonica"> Glass Harmonica <br><input type="radio" name="fav" value="hydraulophone"> Hydraulophone <br>
-    What did you want to learn more about that wasn't in this website? <textarea rows="4" cols="50"></textarea>
+    What did you want to learn more about that wasn't in this website? <textarea rows="4" cols="50"></textarea><br>
     <a href="afterSurvey.php">Submit</a>
+    </div>
 </form>
 </body>
 </html>
